e900f361a0fa211a0dfc22ca758bcb35a3c4aaa2
Janek Brandt <brandt.janek@gmail.com>
2017-05-13 14:47:30 -0700

    convert member-detail to materialize, add default photourl to user creation

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,13 @@
 export class User {
   role: string = 'member';
   bio: string = '';
+  photoURL: string;
 
-  constructor(public displayName: string, public email: string, public photoURL: string, public joined: string) {}
+  constructor(public displayName: string, public email: string, photoURL: string, public joined: string) {
+    if (photoURL) {
+      this.photoURL = photoURL
+    } else {
+      this.photoURL = 'assets/images/profile-default.jpg';
+    }
+  }
 }
