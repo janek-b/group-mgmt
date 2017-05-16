070b3c6cf2e8244f2075fb2ac88cd280e5eb74d7
Janek Brandt <brandt.janek@gmail.com>
2017-05-15 19:05:20 -0700

    remove unnecessary code from edit event component

--- src/app/event-edit/event-edit.component.ts
+++ src/app/event-edit/event-edit.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 import { DbService } from '../db.service';
-import { AuthService } from '../auth.service';
 import { Event } from '../event';
-import { User } from '../user';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-event-edit',
@@ -13,31 +9,15 @@ import * as moment from 'moment';
 })
 export class EventEditComponent implements OnInit {
   @Input() event: any;
-  @Input() user: any;
   @Output() closeSender = new EventEmitter();
-  editEventForm: FormGroup;
 
-  constructor(private db: DbService,
-              private authService: AuthService,
-              private fb: FormBuilder) { }
+  constructor(private db: DbService) { }
 
   ngOnInit() {
-    // this.editEventForm = this.fb.group({
-    //   name: [this.event.name, Validators.required],
-    //   location: [this.event.location, Validators.required],
-    //   date: [moment(this.event.date).format('DD MMMM, YYYY'), Validators.required],
-    //   description: [this.event.description, Validators.required]
-    // })
+
   }
 
   closeEditForm() {
-    this.closeSender.emit();
-  }
-
-  updateEvent() {
-    // var {name, location, date, description} = this.editEventForm.value;
-    // var updateValues = {name: name, location: location, date: (new Date(date).toJSON()), description: description}
-    // this.dbService.updateEvent(this.event.$key, updateValues);
     this.closeSender.emit();
   }
 
